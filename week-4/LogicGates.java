0ea6a1c8afa215fc3420b505e1cbb6f11c9579b8
hcirne <henriquepcirne@gmail.com>
2026-01-21 19:21:01 +0100

    and, or gates implemented

--- week-4/LogicGates.java
+++ week-4/LogicGates.java
@@ -2,6 +2,7 @@ public class LogicGates {
     public static void main(String[] args) {
         
         // AND gate
+        System.out.println("This is the logic gate for AND: ");
         for (int a = 1; a >= 0; a--) {
             for (int b = 1; b >= 0; b--) {
                 System.out.print(a);
@@ -9,5 +10,23 @@ public class LogicGates {
                 System.out.println((a == 1 && b == 1) ? 1 : 0);        
             }
         }
+        System.out.println();
+
+        // OR gate
+        System.out.println("This is the logic gate for OR: ");
+        for (int a = 1; a >= 0; a--) {
+            for (int b = 1; b >= 0; b--) {
+                System.out.print(a);
+                System.out.print(b);
+                System.out.println((a == 1 || b == 1) ? 1 : 0);
+            }
+        }
+        System.out.println();
+
+        // NOT Gate
+        // NAND Gate
+        // NOR Gate
+        // XOR Gate
+        // XNOR Gate
     }
 }
